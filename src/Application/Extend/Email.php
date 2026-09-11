ea3f76a01046dba4d5681eecd2a121fef8403bec
benedikt99-ger <benedikt99@gmail.com>
2026-09-11 15:24:22 +0200

    Add files via upload

--- src/Application/Extend/Email.php
+++ src/Application/Extend/Email.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace nuenemann\sendrequest\Application\Extend;
+namespace nuenemann\widerruf\Application\Extend;
 
 use \OxidEsales\Eshop\Core\Registry;
 use OxidEsales\EshopCommunity\Internal\Framework\Templating\TemplateRendererBridgeInterface;
