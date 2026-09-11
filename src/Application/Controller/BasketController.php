d64051bc1c462b4e5e0a9b27e1fb75b21fa68bc9
benedikt99-ger <benedikt99@gmail.com>
2026-09-11 15:31:28 +0200

    Update BasketController.php

--- src/Application/Controller/BasketController.php
+++ src/Application/Controller/BasketController.php
@@ -22,21 +22,43 @@ class BasketController extends BasketController_parent
 
         if (!$message) {
             // nothing entered, just reload the basket page
+			Registry::getUtilsView()->addErrorToDisplay('SENDREQUEST_ERROR_1', false, true);
             return;
         }
+		
+
+
+        $oUser = $this->getUser();
+        if ($oUser) {
+			$usermail = $oUser->oxuser__oxusername->value;
+			$username = $oUser->oxuser__oxfname->value." ".$oUser->oxuser__oxlname->value;
+			$sLogfile = Registry::getConfig()->getLogsDir() .'bn.log';
+			file_put_contents($sLogfile, trim(date('Y-m-d H:i:s')." user ".$usermail." ".$username).PHP_EOL,FILE_APPEND);			
+        }		
+		
+		$sLogfile = Registry::getConfig()->getLogsDir() .'bn.log';
+		file_put_contents($sLogfile, trim(date('Y-m-d H:i:s')." sendRequest message ".$message).PHP_EOL,FILE_APPEND);			
 		
 		$session = Registry::getSession();
 		$oBasket = $session->getBasket();
 
-        $email = oxNew(Email::class);
-        $email->setSubject('Preis-Anfrage');
-        $email->setBody($this->buildRequestBody($message, $oBasket));
+        $oEmail = oxNew(Email::class);
+		$adata["email"] = $usermail;
+		$adata["name"] = $username;
+		$adata["message"] = $message;
+		$adata["subject"] = "Preis-Anfrage ".$oEmail->getShop()->oxshops__oxname->getRawValue();
+		$adata["body"] = $this->buildRequestBody($message, $oBasket);
+		
+        $x = $oEmail->sendRequestToOwner($adata);
+        $y = $oEmail->sendRequestToUser($adata);
+		
+        // $email->setSubject('Preis-Anfrage');
+        // $email->setBody($this->buildRequestBody($message, $oBasket));
         // $email->setRecipient(Registry::getConfig()->getConfigParam('sAdminEmail'));
-		$email->setRecipient('benedikt@nuenemann.net');
-        $email->setFrom(Registry::getConfig()->getConfigParam('sAdminEmail'));
-        $email->send();
-
-        Registry::getUtilsView()->addErrorToDisplay('MESSAGE_SENT', false, true);
+		// $email->setRecipient('benedikt@nuenemann.net');
+        // $email->setFrom(Registry::getConfig()->getConfigParam('sAdminEmail'));
+        // $email->send();
+        Registry::getUtilsView()->addErrorToDisplay('SENDREQUEST_SUCCESS', false, true);
 
         // stay on basket, or return another controller class name to redirect
         return;
@@ -45,8 +67,8 @@ class BasketController extends BasketController_parent
     protected function buildRequestBody(string $message, $oBasket): string
     {
         $lines   = [];
-        $lines[] = "Message:\n{$message}\n";
-        $lines[] = "Basket contents:";
+        $lines[] = "Kommentar:\n{$message}\n";
+        $lines[] = "Artikel:";
 		
 		// $aBasketArticles = $this->getBasketArticles();
 		// $aBasketArticles = $oBasket->getContents();
@@ -55,6 +77,10 @@ class BasketController extends BasketController_parent
         foreach ($oBasket->getContents() as $sKey =>  $oBasketItem) {
             $article = $oBasketItem->getArticle(false);
             if ($article) {
+				$sLogfile = Registry::getConfig()->getLogsDir() .'bn.log';
+				$line = sprintf('- %s (Art.Nr. %s) x %d',$article->getFieldData('oxtitle'),$article->getFieldData('oxartnum'),$oBasketItem->getAmount());
+				file_put_contents($sLogfile, trim(date('Y-m-d H:i:s')." ".$line).PHP_EOL,FILE_APPEND);	
+				
                 $lines[] = sprintf(
                     '- %s (Art.Nr. %s) x %d',
                     $article->getFieldData('oxtitle'),
